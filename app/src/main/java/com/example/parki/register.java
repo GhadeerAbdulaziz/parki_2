2ece2ac1e85d5af42724a4c2fde64513d32ac3d5
Ghadeer_abdulaziz <46407171+GhadeerAbdulaziz@users.noreply.github.com>
2019-10-24 21:48:49 +0300

    email is alradey taken

--- app/src/main/java/com/example/parki/register.java
+++ app/src/main/java/com/example/parki/register.java
@@ -15,28 +15,29 @@ public class register extends AppCompatActivity {
     DatabaseHelper db;
     Button _btnreg;
     TextView _txtlog;
-    EditText _txtname,_txtemaile,_txtpass,__txtphone,_txtcar,_txtcarnum;
-    boolean isNameValid, isEmailValid, isPhoneValid, isPasswordValid,isCarValid,isCarnumValid;
+    EditText _txtname, _txtemaile, _txtpass, __txtphone, _txtcar, _txtcarnum;
+    boolean isNameValid, isEmailValid, isPhoneValid, isPasswordValid, isCarValid, isCarnumValid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        db=new DatabaseHelper(this);
-        _btnreg =(Button)findViewById(R.id.btnreg);
-        _txtname=(EditText)findViewById(R.id.txtname);
-        _txtemaile=(EditText)findViewById(R.id.txtemail);
-        _txtpass=(EditText)findViewById(R.id.txtpass);
-        __txtphone=(EditText)findViewById(R.id.txtphone);
-        _txtcar=(EditText)findViewById(R.id.txtcar);
-        _txtcarnum=(EditText)findViewById(R.id.txtcarnum);
-        _txtlog=(TextView) findViewById(R.id.txtlog);
+        db = new DatabaseHelper(this);
+        _btnreg = (Button) findViewById(R.id.btnreg);
+        _txtname = (EditText) findViewById(R.id.txtname);
+        _txtemaile = (EditText) findViewById(R.id.txtemail);
+        _txtpass = (EditText) findViewById(R.id.txtpass);
+        __txtphone = (EditText) findViewById(R.id.txtphone);
+        _txtcar = (EditText) findViewById(R.id.txtcar);
+        _txtcarnum = (EditText) findViewById(R.id.txtcarnum);
+        _txtlog = (TextView) findViewById(R.id.txtlog);
 
         _btnreg.setOnClickListener(new View.OnClickListener() {
             @Override
 
-                public void onClick(View v) {
-                    SetValidation();
-                }
+            public void onClick(View v) {
+                SetValidation();
+            }
                 /*
                 String name=_txtname.getText().toString();
                 String email=_txtemaile.getText().toString();
@@ -63,19 +64,20 @@ public class register extends AppCompatActivity {
         _txtlog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent (register.this,MainActivity.class);
+                Intent intent = new Intent(register.this, MainActivity.class);
                 startActivity(intent);
             }
 
 
         });
     }
+
     public void SetValidation() {
         // Check for a valid name.
         if (_txtname.getText().toString().isEmpty()) {
             _txtname.setError(getResources().getString(R.string.name_error));
             isNameValid = false;
-        } else  {
+        } else {
             isNameValid = true;
         }
 
@@ -86,7 +88,7 @@ public class register extends AppCompatActivity {
         } else if (!Patterns.EMAIL_ADDRESS.matcher(_txtemaile.getText().toString()).matches()) {
             _txtemaile.setError(getResources().getString(R.string.error_invalid_email));
             isEmailValid = false;
-        } else  {
+        } else {
             isEmailValid = true;
         }
 
@@ -94,7 +96,7 @@ public class register extends AppCompatActivity {
         if (__txtphone.getText().toString().isEmpty()) {
             __txtphone.setError(getResources().getString(R.string.phone_error));
             isPhoneValid = false;
-        } else  {
+        } else {
             isPhoneValid = true;
         }
 
@@ -105,38 +107,41 @@ public class register extends AppCompatActivity {
         } else if (_txtpass.getText().length() < 6) {
             _txtpass.setError(getResources().getString(R.string.error_invalid_password));
             isPasswordValid = false;
-        } else  {
+        } else {
             isPasswordValid = true;
         }
         if (_txtcar.getText().toString().isEmpty()) {
             _txtcar.setError(getResources().getString(R.string.car_error));
             isCarValid = false;
-        } else  {
+        } else {
             isCarValid = true;
         }
         if (_txtcarnum.getText().toString().isEmpty()) {
             _txtcarnum.setError(getResources().getString(R.string.carnum_error));
             isCarnumValid = false;
-        } else  {
+        } else {
             isCarnumValid = true;
         }
 
         if (isNameValid && isEmailValid && isPhoneValid && isPasswordValid) {
-            String name=_txtname.getText().toString();
-            String email=_txtemaile.getText().toString();
-            String pass=_txtpass.getText().toString();
-            String phone=__txtphone.getText().toString();
-            String car=_txtcar.getText().toString();
-            String carnum=_txtcarnum.getText().toString();
-            db.addUser(name,email,pass,phone,car,carnum);
-            Toast.makeText(getApplicationContext(),"تم التسجيل بنجاح",Toast.LENGTH_LONG).show();
-            Intent movToLogin=new Intent(register.this,MainActivity.class);
-            startActivity(movToLogin);
+            if (!db.checkUser(_txtemaile.getText().toString().trim())) {
+                String name = _txtname.getText().toString();
+                String email = _txtemaile.getText().toString();
+                String phone = __txtphone.getText().toString();
+                String car = _txtcar.getText().toString();
+                String carnum = _txtcarnum.getText().toString();
+                db.addUser(name, email, pass, phone, car, carnum);
+                Toast.makeText(getApplicationContext(), "تم التسجيل بنجاح", Toast.LENGTH_LONG).show();
+                Intent movToLogin = new Intent(register.this, MainActivity.class);
+                startActivity(movToLogin);
+
+            } else {
+                Toast.makeText(getApplicationContext(), "البريد الالكتروني مسجل مسبقا ", Toast.LENGTH_LONG).show();
+            }
 
         }
-
     }
-    }
+}
 
 
 
